6fc5986ba848f1d5763d7b007de671201094010f
thatGuy- <thatGuy-@users.noreply.github.com>
2014-04-10 14:13:11 -0400

    Added to ReadMe, Added Heap Sort

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Searches and Sorts
 
-I implemented some of the more popular sort and search algorithms with generics just for practice.I provided explanations and complexities in the comments just to help out.
+I implemented some of the more popular sort and search algorithms with generics just for practice. I provided algorithm explanations and complexities in the method headers to help out.
+
+I also have extensive commenting in some places just to help refresh the inner workings of the algorithm for myself at a quick glance.  
  
 For the sake of testing I also created a quick Tree and Node class to use with the Depth first and Breadth First Searches.
--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,5 @@
 
 public class Sorts<T extends Comparable<T>> {
-      
 
    /******************************************************
     * 
@@ -8,6 +7,7 @@ public class Sorts<T extends Comparable<T>> {
     * 
     ******************************************************/
 
+    
     /*
      * Method that sorts a given array using the Insertion Sort algorithm.
      * Generally faster than selection sort.
@@ -27,7 +27,6 @@ public class Sorts<T extends Comparable<T>> {
      *  Average, Worst-- O(n^2)
      *
      * @param arr The array of generic objects to sort. 
-     * @return The sorted array.
     */
     public void insertionSort(T[] arr){
 
@@ -42,6 +41,7 @@ public class Sorts<T extends Comparable<T>> {
         }
     }
 
+    
 
    /*
     * Method that sorts a given array using the Selection Sort algorithm. 
@@ -57,7 +57,6 @@ public class Sorts<T extends Comparable<T>> {
     * Best, Average, Worst-- O(n^2)
     *
     * @param arr The array of generic objects to sort. 
-    * @return The sorted array.
     */
     public void selectionSort(T[] arr){
 
@@ -74,6 +73,7 @@ public class Sorts<T extends Comparable<T>> {
     }
 
     
+    
    /* 
     * Method that sorts a given array using the Bubble Sort algorithm. 
     * 
@@ -88,7 +88,6 @@ public class Sorts<T extends Comparable<T>> {
     * Average, Worst-- O(n^2)
     *
     * @param arr The array of generic objects to sort. 
-    * @return The sorted array.
     */
     public void bubbleSort(T[] arr){
 
@@ -110,10 +109,8 @@ public class Sorts<T extends Comparable<T>> {
             length--; 
         }
     }
-
 	
-
-
+    
 
     /***********************************************************************
      * 
@@ -126,7 +123,7 @@ public class Sorts<T extends Comparable<T>> {
 
     
    /* 
-    * Method that sorts the given array using the Mege Sort algorithm
+    * Method that sorts the given array using the Merge Sort algorithm
     * 
     * Comparision based sorting algorithm. (divide and conquer)
     * - Uses a helper function sort() to recursively split the list into 
@@ -137,7 +134,6 @@ public class Sorts<T extends Comparable<T>> {
     * best, average, worst-- O(nlogn)
     *
     * @param arr The array to be sorted
-    * @return The sorted array
    */
     public void mergeSort(T[] arr){
         sort(arr, 0, arr.length - 1);
@@ -145,7 +141,7 @@ public class Sorts<T extends Comparable<T>> {
     
     private void sort(T[] arr, int low, int high){
         if(low < high){
-            int mid = low + (high - low) / 2;
+            int mid = low + ((high - low) / 2);
             sort(arr, low, mid); //left array
             sort(arr, mid + 1, high); //right array
             merge(arr, low, mid, high); //merge them together
@@ -153,7 +149,7 @@ public class Sorts<T extends Comparable<T>> {
     }
     
     private void merge(T[] arr, int low, int mid, int high){
-        Object[] helper = new Object[arr.length];
+        Object[] helper = new Object[(high - low) + 1];
         int i = 0;
         int l = low;
         int r = mid + 1;
@@ -187,11 +183,82 @@ public class Sorts<T extends Comparable<T>> {
         }
         
         //Put the sorted elements from helper back into array.
-        for(int count = 0; count < arr.length; count++){
-            arr[count] = (T) helper[count]; 
+        for(int arrInd = low, helpInd = 0; helpInd < helper.length; arrInd++, helpInd++){
+            arr[arrInd] = (T) helper[helpInd]; 
         }
     }
     
+   
     
-    
+    /* 
+    * Method that sorts the given array using the Heap Sort algorithm
+    * 
+    * - Uses a helper function maxHeapify() to initally build a max heap out of 
+    *   the given array.
+    * - Takes the first (max) element and places it at end. Decreases active 
+    *   array length by one, re-heapifys the array, then repeats the process until
+    *   the active size is one and the array is sorted.
+    * 
+    * best, average, worst-- O(nlogn)
+    *
+    * @param arr The array to be sorted
+   */
+    public void heapSort(T[] arr){
+
+        int heapSize = arr.length;
+
+        //Starts building heap from bottom up. 
+        //(heapSize -1)/2 is the lowest node (on bottom left) that can
+        //possibly have children when using array underlying representation
+        for(int currHeapIndex = (heapSize - 1) /2; currHeapIndex >= 0; currHeapIndex--){
+            maxHeapify(arr, heapSize, currHeapIndex);
+        }
+
+        for(int sortPosition = heapSize - 1; sortPosition > 0; sortPosition--){
+
+            T temp = arr[sortPosition];
+            arr[sortPosition] = arr[0]; //put max at end
+            arr[0] = temp; //last element now at top.
+            heapSize--; //disregard last element now, it's the max
+
+            maxHeapify(arr, heapSize, 0); //Re-Heapify to get new max at 0! 
+        }
+    }
+
+    private void maxHeapify(T[] arr, int heapSize, int index){
+        int left = ((index + 1) * 2) - 1;
+        int right = (index + 1) * 2;
+        int largest = Integer.MIN_VALUE;
+
+        //Need heapSize because we have to make sure child index is less than heap size. 
+        //We are decreasing our working array size each time we call this
+        //(after inital call) due to the current largest element being moved to the end
+        //of the array. (Also possibly on first inital heapify call, we might be  
+        //considering a child of a leaf node aka nothing)
+        
+        //left child bigger
+        if(left < heapSize && arr[left].compareTo(arr[index]) > 0){
+            largest = left;
+        }
+        else{
+            largest = index;
+        }
+        
+        //right child bigger than new largest
+        if(right < heapSize && arr[right].compareTo(arr[largest]) > 0){
+            largest = right;
+        }
+
+        if(largest != index){
+            T temp = arr[largest];
+            arr[largest] = arr[index];
+            arr[index] = temp;
+
+            //Now do again with index of the larger child.
+            //Will work its way down tree again and make sure the previously 
+            //heaped nodes still satisfy max heap property.
+            maxHeapify(arr, heapSize, largest);
+        }
+
+    }
 }
